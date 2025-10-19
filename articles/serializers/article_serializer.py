e85804ae2ef25529b368e5740837f7ea4c8fc8db
Ojkee <17oju17@gmail.com>
2025-10-19 21:26:20 +0200

    2025-10-19; 6; change date format

--- articles/serializers/article_serializer.py
+++ articles/serializers/article_serializer.py
@@ -3,6 +3,8 @@ from articles.models import Article
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    publish_date = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S", required=False)  # type: ignore
+
     class Meta:
         model = Article
         fields = "__all__"
